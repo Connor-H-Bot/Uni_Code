7869a8a35efdd9c3a13f6d0db541179498d29e6e
Connor_Bot <60994277+Connor-H-Bot@users.noreply.github.com>
2022-03-15 00:22:02 +0800

    Update lab1_fibb_seq.py

--- PYTHON1401/LABS/LAB01/lab1_fibb_seq.py
+++ PYTHON1401/LABS/LAB01/lab1_fibb_seq.py
@@ -1,17 +1,22 @@
-x = int(input("Enter a number to find its fibonacci sequence: "))
+#"Whats the ~th number of the Fibonacci sequence? This script will tell you"
+
+x = int(input("Enter a number to find its Fibonacci sequence: "))
  
-def fibb_seq(x):
-    iteration = x
-    counter = 0
-    fib_list = [x]
-    for counter in x:
+def fibb_seq(x):     #Method to find the number
+    counter = 0      
+    fib_list = []
+
+    if x <= 0:      #If spec number is 0 or negative
+        return 0
+
+    while counter < x:
         if counter == 0:
-            fib_list[counter] = 0
-            fib_list[counter + 1] = 1
+            fib_list.insert(counter, 0)
+            fib_list.insert((counter + 1), 1)
             counter += 2
         else:
-            fib_list[counter] == (fib_list[counter - 1] + fib_list[counter - 2])
+            fib_list.insert(counter, (fib_list[counter - 1] + fib_list[counter - 2]))
             counter += 1
-    return fib_list[x]
+    return fib_list[x - 1]
 
 print(fibb_seq(x))
