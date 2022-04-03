205292027e3159605be3e6e8490eac05a7df4a60
Connor_Bot <60994277+Connor-H-Bot@users.noreply.github.com>
2022-04-03 21:35:14 +0800

    Update lab_03_loops.py

--- PYTHON1401/LABS/LAB03/lab_03_loops.py
+++ PYTHON1401/LABS/LAB03/lab_03_loops.py
@@ -32,3 +32,77 @@ def bmi_risk(bmi, age):
         return "Medium"
     else:
         return "High"
+
+#4: input cost and savings. If the cost is < 5% of savings return true, else return false
+def buy_goods(cost, savings):
+    if (cost or savings) <= 0: #get rid of any 0 or negative inputs
+        return False
+    elif (savings * 0.05) > cost:
+        return True
+    else:
+        return False
+
+#5: Return "found him" if inputs match predetermined AGL (Age, Gender, Location) 
+def record_check(age, gender, location):
+    if ((age > 18) and gender == "M" and (location == ("Perth") or location == ("Sydney"))):
+        print("Found him!")
+    else: 
+         print("Did not find him.")
+
+#6: Return list if length is even, or repeats the final element and returns
+def balance_list(items):
+    list_length = len(items)
+    if (list_length % 2) == 0:
+        return items
+    else:
+        items.append(items[list_length - 1])
+        return items
+
+#7: Repeat an entire list if length is even, or repeat final element if length is odd
+def double_list(items):
+    list_length = len(items)
+    if (list_length % 2) == 0:
+        items.extend(items)
+        return items
+    else:
+        items.append(items[list_length - 1])
+        return items
+
+#8: Return true if two input words are anagrams, otherwise false
+def are_anagrams(word1, word2):
+    if word1 == word2:
+        return False
+    word1_list = list(word1)
+    word1_list.sort()
+    word2_list = list(word2)
+    word2_list.sort()
+    if word1_list == word2_list:
+        return True
+    else:
+        return False
+
+#9: Return index if age and name have a match. No error handling needed 
+def locate_person(age_list, name_list, age, name):
+    index = 0
+    while index < len(age_list):
+        if (age_list[index] == age) and (name_list[index] == name):
+            return index
+        else:
+            index += 1
+
+#10: Returns ammount of animals hunted based on weather, num of bullets used and animal type
+def hunting_animals(weather, animal, n):
+    if weather == "sunny" and animal == "rabbit":
+        return n
+    elif (weather == "rainy" and animal == "rabbit") or (weather == "sunny" and animal == "deer"):
+        return int(n/2)
+    elif weather == "rainy" and animal == "deer":
+        return int(n/3)
+
+#11: converts temp from cel to fahr and checks if its more or less than the limit
+def check_temperature(temperature, limit):
+    fahrenheit = temperature * 9 / 5 + 32
+    if fahrenheit < limit:
+        return True
+    else:
+        return False
