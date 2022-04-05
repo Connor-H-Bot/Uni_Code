e57dd802f3e824856caa7583c4f971b1eb1ce911
Connor_Bot <60994277+Connor-H-Bot@users.noreply.github.com>
2022-04-05 10:12:08 +0800

    Update find_longer.sh

--- CITS2003/week4/find_longer.sh
+++ CITS2003/week4/find_longer.sh
@@ -1,29 +1,29 @@
 #!/bin/bash 
 
-#if two inputs are not put in for comparison...
-if [[ $# -ne 2 ]]
-then 
-    echo "ERROR: Script requires 2x arguments"
+# if two inputs are not put in for comparison...
+if [[ -z $1 ]] || [[ -z $2 ]] 
+then
+    1>&2 echo "ERROR: Script requires 2x arguments"
+    exit 2
+elif [[ ! -f $1 ]]
+then
+    1>&2 echo "ERROR: Cannot find $1"
     exit 1
-
-#If one input doesnt exist
-[elif [[ ! -f $# ]]
-then 
-    echo "ERROR: One or more arguments don't exist"
-    exit 1]
-
-#Finally, if it does exist
-[else 
+elif [[ ! -f $2 ]]
+then
+    1>&2 echo "ERROR: Cannot find $2"
+    exit 1
+else
+    # both exist so record lengths
     file_1_len=$(cat $1 | wc -l)
     file_2_len=$(cat $2 | wc -l)
-    if [[ file_1_len -eq file_2_len ]] 
+    if [[ $file_1_len -eq $file_2_len ]]
+    then
         echo "Equal length!"
-        exit 0
-    [elif [[ file_1_len -gt file_2_len ]]
+    elif [[ $file_1_len -gt $file_2_len ]]
+    then
         echo "$1 is bigger!"
-        exit 0]
-    [else
+    else
         echo "$2 is bigger!"
-        exit 0]
-]
+    fi
 fi
