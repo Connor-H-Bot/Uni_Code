5b38855dd9428a967678fcc502c54859fe5272c8
Connor_Bot <60994277+Connor-H-Bot@users.noreply.github.com>
2022-04-19 23:18:35 +0800

    Update 23208009.py

--- PYTHON1401/LABS/project_1/23208009.py
+++ PYTHON1401/LABS/project_1/23208009.py
@@ -13,7 +13,8 @@
 def main(csvfile, adults, type):
     x = get_adult(csvfile, adults)
     y = make_2d(x)
-    return (y)
+    z = asymmetry_analysis(y)
+    return z
 
 #Retrieves selected adult from the csvfile
 def get_adult(filename, substring):
@@ -45,4 +46,24 @@ def make_2d(list):
         input_list_index += 1
     return list_in_2d  
 
-print(main('asymmetry_sample.csv', 'A1356', True))
+# Calibrate data and perform asymmetry analysis
+def asymmetry_analysis(list_in_2d):
+    float_list = [[float(element) for element in list_inner] for list_inner in list_in_2d] #convert all numbers to floats
+    asymmetry_analysis_list = [] #list to store values for each facial points analysis
+    if (float_list[9][0] or float_list[9][1] or float_list[9][2]) != 0: #Check if nose has any values != 0
+        x_offset, y_offset, z_offset = float_list[9][0], float_list[9][1], float_list[9][2]
+        del float_list[9]
+        for index in range(len(float_list)):
+            float_list[index][0] -= x_offset
+            float_list[index][1] -= y_offset
+            float_list[index][2] -= z_offset
+    else: #remove nose elements from list
+        del float_list[9]
+    analysis_index = 0
+    while analysis_index < len(float_list): #Iterate through entire list and use asymmetry formula for each point
+        x_value, y_value, z_value = float_list[analysis_index][0], float_list[analysis_index][1], float_list[analysis_index][2]
+        asymmetry_analysis_list.append((x_value**2 + y_value**2 + z_value**2) ** 0.5) #sqrt(x^2 + y^2 + z^2)
+        analysis_index += 1
+    return asymmetry_analysis_list
+
+print(main('asymmetry_sample.csv', 'C4996', 'stats'))
