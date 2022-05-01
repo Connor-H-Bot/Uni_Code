a1742da09838dd500e64809d808200c3991de919
Connor_Bot <60994277+Connor-H-Bot@users.noreply.github.com>
2022-05-01 13:17:16 +0800

    Update malaria_incidence.sh

--- CITS2003/project_1/malaria_incidence.sh
+++ CITS2003/project_1/malaria_incidence.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-#Test if there is input
-if [[ ! -z $1 ]]
+if [[ ! -z $1 ]] #Test if there is input
 then
-    if [[ 1999 < $1 && $1 < 2019 && $1 < 0 ]] #Checks the input is within the year range && $1 < 2019, and is actually a number
+    if [[ $1 == 20** && $1 < 2019 ]] #Checks the input is an int between 2000-2018
     then
         unluckiest_country=$(cat incedenceOfMalaria.csv | grep $1 | cut -d ',' -f 1,4 | tr ',' ':' | sort -k2 -t':' -nr  | head -n 1)
         country_figure=$(echo $unluckiest_country | cut -f2 -d':')
@@ -11,17 +10,24 @@ then
         if [[ ! -z $country_name && 0 < $country_figure ]] #Makes sure there is data in country name and a number for the figure
         then
             echo "The hardest hit country in $1 was $country_name with $country_figure infections per thousand. "
+            exit 0
         else
-            echo "ERROR: The file does not contain data for this year. "
+            echo "ERROR: This year does not have valid data. " 
             exit 1
         fi
-    elif [[ 0 == 0 ]]
-        echo "OKAY"
-    fi
-    else
-        echo "ERROR: Data only ranges from 2000-2018. "
+    elif [[ $1 == [a-z]* || $1 == [A-Z]* ]] #Check if input is a string
+    then
+        echo "This works: $1 "
+        hardest_year_and_stats=$(cat incedenceOfMalaria.csv | grep "$1" | cut -d ',' -f 3,4 | tr ',' ':' | sort -k2 -t':' -nr | head -n 1)
+        highest_stat=$(echo $hardest_year_and_stats | cut -f2 -d':')
+        year=$(echo $hardest_year_and_stats | cut -f1 -d':')
+        echo "$1's worst year was $year, with infection rates of $highest_stat per thousand."
+        exit 0
+    elif [[ $1 == [1-9]* ]] #Error case for invalid year
+    then
+        echo "ERROR: Script only takes years from 2000-2018. "
         exit 1
-    fi
+    fi 
 else
     echo "ERROR: Please check your input and try again. "
     exit 1
