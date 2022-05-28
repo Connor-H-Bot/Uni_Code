8ded354628f328409cd9277e476ba3d954d11f3d
Connor_Bot <60994277+Connor-H-Bot@users.noreply.github.com>
2022-05-29 07:52:12 +0800

    Add files via upload

---
+++ RDBMS1402/project_2/23208009.py
@@ -0,0 +1,9 @@
+# Author: Connor Harris
+#
+# Project 2 for CITS1401. 
+#
+#   Performs a facial recognition of two faces and returns the euclidian distance
+
+def main(csv, adults_array):
+    #TODO add checks for if csv exists and two valid inputs to array
+    return
