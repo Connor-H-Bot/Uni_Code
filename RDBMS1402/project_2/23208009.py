72cfc25145d9598f49002177669f0430494cf158
Connor_Bot <60994277+Connor-H-Bot@users.noreply.github.com>
2022-05-29 17:18:36 +0800

    Delete RDBMS1402/project_2 directory

--- RDBMS1402/project_2/23208009.py
+++
@@ -1,9 +0,0 @@
-# Author: Connor Harris
-#
-# Project 2 for CITS1401. 
-#
-#   Performs a facial recognition of two faces and returns the euclidian distance
-
-def main(csv, adults_array):
-    #TODO add checks for if csv exists and two valid inputs to array
-    return
